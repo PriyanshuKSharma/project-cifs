23489412910f325c1d025cec458b9c10e93c9a20
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-10-22 12:28:10 +0530

    Add main Express.js application server with middleware setup

---
+++ app.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const session = require('express-session');
+const flash = require('express-flash');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Session configuration
+app.use(session({
+  secret: 'cybercrime-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 24 * 60 * 60 * 1000 } // 24 hours
+}));
+
+app.use(flash());
+
+// Global variables for templates
+app.use((req, res, next) => {
+  res.locals.user = req.session.user || null;
+  res.locals.messages = req.flash();
+  next();
+});
+
+// Routes
+const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/user');
+const adminRoutes = require('./routes/admin');
+
+app.use('/', authRoutes);
+app.use('/', userRoutes);
+app.use('/admin', adminRoutes);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
