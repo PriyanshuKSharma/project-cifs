ff708fa20d06b7333f4d1cfa12510466c3353b1d
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-11-24 08:10:28 +0530

    feat: integrate Tailwind CSS build pipeline

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx}',
+    './views/**/*.ejs',
+    './public/**/*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
